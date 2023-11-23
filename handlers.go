4cd19a68c32f6eddb209a28982414a824671249d
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2023-11-23 13:11:24 -0500

    updated html

--- handlers.go
+++ handlers.go
@@ -9,8 +9,8 @@ import (
 func HandlerGetHome(w http.ResponseWriter, req *http.Request) {
 
 	log.Println("Get Home Handler: Enter")
-
 	w.WriteHeader(http.StatusOK)
+
 	// w.Write([]byte("Welcome to Swath\n"))
 	tmpl := template.Must(template.ParseFiles("htmx/index.html"))
 	tmpl.Execute(w, nil)
@@ -22,8 +22,8 @@ func HandlerGetHome(w http.ResponseWriter, req *http.Request) {
 func HandlerSanity(w http.ResponseWriter, req *http.Request) {
 
 	log.Println("Sanity Handler: Enter")
-
 	w.WriteHeader(http.StatusOK)
+
 	w.Write([]byte("Server is sane\n"))
 
 	log.Println("Sanity Handler: Exiting")
