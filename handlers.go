fb71efa1f0446b96b7217d9bf442e3b9e2de3ba8
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2023-12-05 17:24:26 -0500

    work in progress to make past swaths

--- handlers.go
+++ handlers.go
@@ -27,10 +27,40 @@ func HandlerGetHome(w http.ResponseWriter, req *http.Request) {
 func HandlerSanity(w http.ResponseWriter, req *http.Request) {
 
 	log.Println("Sanity Handler: Enter")
-	w.WriteHeader(http.StatusOK)
 
 	w.Write([]byte("Server is sane\n"))
+	w.WriteHeader(http.StatusOK)
 
 	log.Println("Sanity Handler: Exiting")
+
+}
+
+func HandlerGetLinks(w http.ResponseWriter, req *http.Request) {
+
+	log.Println("GetLinks Handler: Enter")
+
+	if req.Header.Get("HX-Request") == "true" {
+		w.WriteHeader(http.StatusOK)
+		log.Println("Sending Links")
+
+		tmpl := template.Must(template.ParseFiles("htmx/links.html"))
+		data := []string{
+			"0.0.1",
+			"0.0.3",
+			"0.1.0",
+			"0.1.1",
+			"0.1.2",
+			"0.1.3",
+			"0.1.4",
+		}
+		tmpl.Execute(w, data)
+
+	} else {
+
+		w.WriteHeader(http.StatusForbidden)
+		log.Println("Direct access to /getlinks forbidden")
+	}
+
+	log.Println("GetLinks Handler: Exiting")
 
 }
